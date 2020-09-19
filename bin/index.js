b7189a1e78bfd33b5e81cac4f5aa713851bf96d7
mac <mac@macdeMac-7.local>
2020-09-19 15:58:55 +0800

    集成docker ci

--- bin/index.js
+++ bin/index.js
@@ -8,86 +8,103 @@ const chalk = require('chalk'); // 命令行输出字符颜色
 const figlet = require('figlet');
 const exec = require('promise-exec');
 const currentPath = process.cwd(); // 当前目录路径
-const templatePath = path.resolve(__dirname, '../template/');
 
-async function createTemplate(config) {
-  const projectName = config.projectName;
-  const spinner = ora('正在生成...').start();
-  if (config.projectType === 'Web+API') {
-    try {
-      await fs.copy(
-        path.resolve(`${templatePath}/Web`),
-        path.resolve(`${currentPath}/${projectName}/client`)
-      );
-      await fs.copy(
-        path.resolve(`${templatePath}/API`),
-        path.resolve(`${currentPath}/${projectName}/server`)
-      );
-      await fs.remove(
-        path.resolve(
-          `${currentPath}/${projectName}/server/src/public/stylesheets`
-        )
-      );
-      spinner.stop();
-      ora(chalk.green('目录生成成功！')).succeed();
-      let packageJsonPath = path.resolve(
-        `${currentPath}/${config.projectName}/client`,
-        'package.json'
-      );
-      await handlePackageJson(packageJsonPath, config);
-      packageJsonPath = path.resolve(
-        `${currentPath}/${config.projectName}/server`,
-        'package.json'
-      );
-      await handlePackageJson(packageJsonPath, config);
-      await handlePackageScript(packageJsonPath);
-      if (config.isNpmInstall) {
-        const spinnerInstall = ora('安装依赖中...').start();
-        await exec('cnpm install', {
-          cwd: `${currentPath}/${projectName}/client`,
-        });
-        await exec('cnpm install', {
-          cwd: `${currentPath}/${projectName}/server`,
-        });
-        spinnerInstall.stop();
-        ora(chalk.green('相赖安装成功！')).succeed();
-        successConsole(config);
-      } else {
-        successConsole(config);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  } else {
-    try {
-      await fs.copy(
-        path.resolve(`${templatePath}/${config.projectType}`),
-        path.resolve(`${currentPath}/${projectName}`)
-      );
-      spinner.stop();
-      ora(chalk.green('目录生成成功！')).succeed();
-      const packageJsonPath = path.resolve(
-        `${currentPath}/${config.projectName}`,
-        'package.json'
-      );
-      await handlePackageJson(packageJsonPath, config);
-
-      if (config.isNpmInstall) {
-        const spinnerInstall = ora('安装依赖中...').start();
-        await exec('cnpm install', {
-          cwd: `${currentPath}/${projectName}`,
-        });
-        spinnerInstall.stop();
-        ora(chalk.green('相赖安装成功！')).succeed();
-        successConsole(config);
-      } else {
-        successConsole(config);
-      }
-    } catch (error) {
-      console.error(error);
-    }
+async function createProject(templatePath, distPath) {
+  try {
+    await fs.copy(templatePath, distPath);
+  } catch (error) {
+    chalk.red(error.message);
   }
 }
+
+async function createTemplate(config) {
+  try {
+    const projectName = config.projectName;
+    const spinner = ora('正在生成...').start();
+    let isWeb = config.projectType.indexOf('Web') !== -1;
+    let isAPI = config.projectType.indexOf('API') !== -1;
+    if (isWeb && isAPI) {
+      const templateWebPath = path.resolve(__dirname, '../template/Web');
+      const templateAPIPath = path.resolve(__dirname, '../template/API');
+      const distClientPath = path.resolve(
+        `${currentPath}/${projectName}/client`
+      );
+      const distServerPath = path.resolve(
+        `${currentPath}/${projectName}/server`
+      );
+      await createProject(templateWebPath, distClientPath);
+      await createProject(templateAPIPath, distServerPath);
+      let packageJsonPath = path.resolve(distClientPath, 'package.json');
+      await handlePackageJson(packageJsonPath, config);
+      packageJsonPath = path.resolve(distServerPath, 'package.json');
+      await handlePackageJson(packageJsonPath, config);
+      await handlePackageScript(packageJsonPath);
+      spinner.stop();
+      ora(chalk.green('目录生成成功！')).succeed();
+      if (config.isNpmInstall) {
+        const spinnerInstall = ora('安装依赖中...').start();
+        await exec('cnpm i', {
+          cwd: distClientPath,
+        });
+        await exec('cnpm i', {
+          cwd: distServerPath,
+        });
+        spinnerInstall.stop();
+        ora(chalk.green('相赖安装成功！')).succeed();
+        successConsole(config);
+      } else {
+        successConsole(config);
+      }
+    } else {
+      const templatePath = path.resolve(
+        __dirname,
+        `../template/${config.projectType}`
+      );
+      const distPath = path.resolve(`${currentPath}/${projectName}`);
+      await createProject(templatePath, distPath);
+      const packageJsonPath = path.resolve(distPath, 'package.json');
+      await handlePackageJson(packageJsonPath, config);
+      spinner.stop();
+      ora(chalk.green('目录生成成功！')).succeed();
+      if (isWeb) {
+        if (!config.enableCI) {
+          await fs.remove(path.resolve(distPath, '.gitlab-ci.yml'));
+        }
+      }
+      if (isAPI) {
+        if (!config.enableCI) {
+          await fs.remove(path.resolve(distPath, '.gitlab-ci.yml'));
+          await fs.remove(path.resolve(distPath, '.gitlab-ci-docker.yml'));
+        } else {
+          if (!config.enableDocker) {
+            await fs.remove(path.resolve(distPath, '.gitlab-ci-docker.yml'));
+            await fs.remove(path.resolve(distPath, 'pm2.json'));
+          } else {
+            await fs.remove(path.resolve(distPath, '.gitlab-ci.yml'));
+            await fs.move(
+              path.resolve(distPath, '.gitlab-ci-docker.yml'),
+              path.resolve(distPath, '.gitlab-ci.yml')
+            );
+          }
+        }
+      }
+      if (config.isNpmInstall) {
+        const spinnerInstall = ora('安装依赖中...').start();
+        await exec('cnpm i', {
+          cwd: distPath,
+        });
+        spinnerInstall.stop();
+        ora(chalk.green('相赖安装成功！')).succeed();
+        successConsole(config);
+      } else {
+        successConsole(config);
+      }
+    }
+  } catch (error) {
+    chalk.red(error.message);
+  }
+}
+
 function handlePackageScript(packageJsonPath) {
   const spinner = ora('修改server script...').start();
   const promise = new Promise((resolve, reject) => {
@@ -117,6 +134,7 @@ function handlePackageScript(packageJsonPath) {
   });
   return promise;
 }
+
 function handlePackageJson(packageJsonPath, config) {
   const spinner = ora('正在写入package.json...').start();
   const promise = new Promise((resolve, reject) => {
--- libs/inquirer.js
+++ libs/inquirer.js
@@ -9,7 +9,11 @@ module.exports = {
         type: 'list',
         message: '选择模板:',
         name: 'projectType',
-        choices: ['Web', 'API', 'Web+API'],
+        choices: [
+          { name: '纯前端项目', value: 'Web' },
+          { name: 'API项目', value: 'API' },
+          { name: '前后端不分离(Web+API)', value: 'Web+API' },
+        ],
       },
       {
         name: 'projectName',
@@ -33,6 +37,21 @@ module.exports = {
         message: '是否安装依赖包(cnpm)?',
         name: 'isNpmInstall',
         default: true,
+      },
+      {
+        type: 'confirm',
+        message: '是否集成gitlab-ci?',
+        default: true,
+        name: 'enableCI',
+      },
+      {
+        type: 'confirm',
+        message: '是否打包成docker镜像?',
+        default: true,
+        name: 'enableDocker',
+        when(answer) {
+          return answer.projectType === 'API' && answer.enableCI;
+        },
       },
     ];
     return inquirer.prompt(questions);
